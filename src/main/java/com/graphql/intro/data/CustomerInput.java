7fc730d9348c204ffc23514f4c1c6193d3a4a02e
George Panaretos <georgepanaretos.cac@gmail.com>
2025-03-14 18:12:16 +0100

    Cleanup

--- src/main/java/com/graphql/intro/data/CustomerInput.java
+++ src/main/java/com/graphql/intro/data/CustomerInput.java
@@ -1,5 +1,8 @@
 package com.graphql.intro.data;
 
+import lombok.Data;
+
+@Data
 public class CustomerInput {
     private String firstName;
     private String lastName;
@@ -9,70 +12,6 @@ public class CustomerInput {
     private String city;
     private String state;
     private String zipCode;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 
     public Customer getCustomerEntity(){
         Customer customer = new Customer();
