c2eb92eeb0cc1708be3db7085d80b394bed15af9
George Panaretos <georgepanaretos.cac@gmail.com>
2025-03-14 18:04:51 +0100

    GraphQL Mutation
    Additional changes to run smoothly

--- src/main/java/com/graphql/intro/Application.java
+++ src/main/java/com/graphql/intro/Application.java
@@ -1,13 +1,28 @@
 package com.graphql.intro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories("com.graphql.intro.repo")  // Ensure correct package scan
+@ComponentScan(basePackages = "com.graphql.intro") // Ensure scanning
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+//	@Bean
+//	CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//		return args -> {
+//			System.out.println("Available Spring Beans:");
+//			for (String beanName : ctx.getBeanDefinitionNames()) {
+//				System.out.println(beanName);
+//			}
+//		};
+//	}
 }
--- src/main/java/com/graphql/intro/controller/CustomerController.java
+++ src/main/java/com/graphql/intro/controller/CustomerController.java
@@ -2,8 +2,11 @@ package com.graphql.intro.controller;
 
 
 import com.graphql.intro.data.Customer;
+import com.graphql.intro.data.CustomerInput;
 import com.graphql.intro.repo.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Controller;
 public class CustomerController {
     private final CustomerRepository customerRepository;
 
+    @Autowired
     public CustomerController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
@@ -28,5 +32,10 @@ public class CustomerController {
     @QueryMapping
     public Customer customerByEmail(@Argument String email) {
         return this.customerRepository.findCustomerByEmail(email);
+    }
+
+    @MutationMapping
+    public Customer addCustomer(@Argument(name="input") CustomerInput customerInput){
+        return this.customerRepository.save(customerInput.getCustomerEntity());
     }
 }
